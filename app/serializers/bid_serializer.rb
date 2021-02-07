1fdd786679e55c0325dc67a0ddcdec8c0ee76f7b
gooseisdead <gooseisdead@mac.com>
2021-02-07 14:34:25 -0500

    working serializer for created attribute

--- app/serializers/bid_serializer.rb
+++ app/serializers/bid_serializer.rb
@@ -1,5 +1,5 @@
 class BidSerializer < ActiveModel::Serializer
-  attributes :id, :years, :salary_per_year, :users
+  attributes :id, :years, :salary_per_year, :created_at, :users
   belongs_to :player
   has_many :user_bids
   has_many :users, through: :user_bids
