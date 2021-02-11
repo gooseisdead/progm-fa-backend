62175b0cc3358d1f9e1115cf86c99b21b18de275
gooseisdead <gooseisdead@mac.com>
2021-02-11 18:57:49 -0500

    dinner

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,9 @@
 class Bid < ApplicationRecord
   belongs_to :player
   belongs_to :user
+
+  def nice_timestamp_for_tommy_lasorda
+    self.created_at.strftime("%m/%d/%Y at %I:%M%p")
+  end
+
 end
--- app/serializers/bid_serializer.rb
+++ app/serializers/bid_serializer.rb
@@ -1,5 +1,6 @@
 class BidSerializer < ActiveModel::Serializer
-  attributes :id, :years, :salary_per_year, :created_at, :player_id, :user_id
+  attributes :id, :years, :salary_per_year, :created_at, :player_id, :user_id, :nice_timestamp_for_tommy_lasorda
+
   belongs_to :player
   belongs_to :user
 end
