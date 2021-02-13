a24315da5a1b9febab710011a4fcbeae91180223
gooseisdead <gooseisdead@mac.com>
2021-02-13 12:15:00 -0500

    sat

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,5 +1,5 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :position, :real_mlb_team, :years, :salary_per_year, :minor_league_status
+  attributes :id, :name, :position, :real_mlb_team, :years, :salary_per_year, :minor_league_status, :bid_ids
   has_one :team
   has_many :bids
 end
